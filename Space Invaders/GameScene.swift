5aea50c9364ac8793b7981d38b22430706f8e741
wissam <wissam1218@gmail.com>
2021-01-01 14:47:28 -0500

    Can now fire bullet

--- Space Invaders/GameScene.swift
+++ Space Invaders/GameScene.swift
@@ -9,6 +9,8 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    let player = SKSpriteNode(imageNamed: "playerShip")
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -16,10 +18,26 @@ class GameScene: SKScene {
         background.position = CGPoint(x: self.size.width/2 ,y: self.size.height/2)
         background.zPosition = 0;
         self.addChild(background)
-        let player = SKSpriteNode(imageNamed: "playerShip")
+        
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height*0.2)
         player.zPosition = 2;
         self.addChild(player)
+    }
+    func fireBullet(){
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([moveBullet,deleteBullet])
+        bullet.run(bulletSequence)
+        
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
     }
 }
